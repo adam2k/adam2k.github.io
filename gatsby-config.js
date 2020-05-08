5f51a4637e2865b258570ac0306373710164a90c
Adam Vandover <avandover@gmail.com>
2020-05-08 10:03:33 -0400

    Update website framework to gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Adam Vandover`,
+    subTitle: '// Senior Software Engineer',
+    description: `My personal website.`,
+    author: `@adam2k`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#febc20`,
+        theme_color: `#febc20`,
+        display: `minimal-ui`,
+        icon: `src/assets/adam-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
