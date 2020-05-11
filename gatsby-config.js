9467df8f908af78aef7473f0e6ef2355210af65f
Adam Vandover <avandover@gmail.com>
2020-05-10 21:40:33 -0400

    Add webfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,6 @@ module.exports = {
         icon: `src/assets/adam-icon.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
