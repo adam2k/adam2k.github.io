aa03bb48bd48851ef748d212466ca704b0d4cd61
Adam Vandover <adam@astronomer.io>
2021-05-15 11:43:01 -0400

    Update landing page and resume

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,28 @@ const IndexPage = () => (
     <div className="description">
       <p>
         Hello, I am a software engineer from Virginia. I work with a wide range 
-        of technologies from scheduling &amp; orchestration tool
-        &nbsp;
-        <a href="https://kubernetes.io/" target="_blank">
-          Kubernetes (K8s)
+        of technologies from&nbsp;
+        <a href="https://www.typescriptlang.org" target="_blank">
+          typescript
         </a>
-        &nbsp;to frontend frameworks&nbsp;
-        <a href="https://reactjs.org/" target="_blank">React</a>
         &nbsp;&amp;&nbsp;
-        <a href="https://vuejs.org/" target="_blank">
-          Vue.js
+        <a href="https://nodejs.org/" target="_blank">
+          node.js
+        </a>
+        &nbsp;to&nbsp;
+        <a href="http://apollographql.com/" target="_blank">
+          Apollo Server
+        </a>,&nbsp;
+        <a href="https://www.prisma.io" target="_blank">
+          Prisma
+        </a>
+        &nbsp;&amp;&nbsp;
+        <a href="https://golang.org" target="_blank">
+          go lang
         </a>.
       </p>
       <p>
-        I am a musician, public speaker, collaborator and creative. I am driven to create
+        I am a dad, husband, runner, musician, public speaker, collaborator and creative. I am driven to create
         innovative, inclusive software solutions that help improve lives.
       </p>
       <p>Interested in talking? Send me a message and we can chat.</p>
